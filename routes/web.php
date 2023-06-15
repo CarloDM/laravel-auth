7a534b5aee6bad834ed86fc67ac526f40997e0c1
carloDM <carlo.de.mauro.work@gmail.com>
2023-06-15 17:03:36 +0200

    admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\PageController;
-
+use App\Http\Controllers\Admin\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,18 +15,14 @@ use App\Http\Controllers\Guest\PageController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/', [PageController::class, 'index'])->name('home');
+//
+// raggruppare le rotte
+Route::middleware(['auth','verified'])
+->name('admin.')
+->prefix('admin')
+->group(function(){
+    route::get('/', [DashboardController::class, 'index'])->name('home');
 });
 
 require __DIR__.'/auth.php';
